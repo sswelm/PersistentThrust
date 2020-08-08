e59041d27f31c4abea72e1cb25d6c7e565ec16d3
Standecco <dlombardo32@gmail.com>
2020-08-08 11:50:53 +0200

    Update Unity project

---
+++ Unity/PTUI/Assets/AutoSave/Editor/AutoSaveExtension.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutoSaveExtension
+{
+    static AutoSaveExtension()
+    {
+        EditorApplication.playModeStateChanged += AutoSaveWhenPlaymodeStarts;
+    }
+
+    private static void AutoSaveWhenPlaymodeStarts(PlayModeStateChange state)
+    {
+        if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+        {
+            EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene(), null, false);
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
